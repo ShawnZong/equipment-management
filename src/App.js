658ef5da692d253c3699fc5028ff2aafac0bee96
tanj <junsheng.tan@eurecom.fr>
2022-04-30 08:47:57 +0300

    add 404 notfound route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import { Table } from "./components/Table";
+import { Routes, Switch, Route } from "react-router-dom";
+import { NotFound } from "./components/NotFound";
+
 function App() {
   return (
     <div className="App">
-      <Table />
+      <Routes>
+        <Route path="/" element={<Table />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
